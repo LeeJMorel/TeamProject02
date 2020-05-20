3b3c09bc76f66a3b3588d1d42290ec4c209b9629
Lee L Janzen <46356258+leejanzen91@users.noreply.github.com>
2020-05-20 11:18:09 -0700

    Update AbstractList.java

--- AbstractList.java
+++ AbstractList.java
@@ -1,1 +1,8 @@
-Does code stuff or whatever
+/*CS211 SPRING 2020, Created: 5/20/20, Lee Janzen, Harold Pham
+This is the abstract list class.
+Created for ch16, programming project 3.*/
+import java.util.*;
+import java.io.*;
+
+public abstract class AbstractList<E> implements List<E> { {
+}
