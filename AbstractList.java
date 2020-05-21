8f4749b135fdde3cc96e9c9a33b939d4d6eb2edb
Lee L Janzen <46356258+leejanzen91@users.noreply.github.com>
2020-05-21 09:56:30 -0700

    Update AbstractList.java

--- AbstractList.java
+++ AbstractList.java
@@ -4,5 +4,36 @@ Created for ch16, programming project 3.*/
 import java.util.*;
 import java.io.*;
 
-public abstract class AbstractList<E> implements List<E> { {
+public abstract class AbstractList<E> implements List<E> {
+   private int size;        // current number of elements in the list
+
+   public int size() {
+        return size;
+    }
+   
+   // post: returns true if list is empty, false otherwise 
+   public boolean isEmpty() {
+        return size == 0;
+   }
+
+}
+
+private interface AbstractListIterator<E> extends Iterator<E> {
+  private int position;           // current position within the list
+  private boolean removeOK;       // whether it's okay to remove now
+  
+   // post: constructs an iterator for the given list
+   public AbstractListIterator() {
+      position = 0;
+      removeOK = false;
+   }
+        
+   boolean hasNext(){
+   }
+   
+   E next(){
+   }
+   
+   void remove(){
+   }
 }
