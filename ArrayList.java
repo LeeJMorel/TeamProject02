34c402271a5da59143e975c080988b6e667910fb
Lee L Janzen <46356258+leejanzen91@users.noreply.github.com>
2020-05-22 20:48:46 -0700

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,10 +1,7 @@
-// Class ArrayList<E> can be used to store a list of values of type E.
-
 import java.util.*;
 
 public class ArrayList<E> extends AbstractList<E> implements List<E>{
     private E[] elementData; // list of values
-    private int size;        // current number of elements in the list
 
     public static final int DEFAULT_CAPACITY = 100;
 
@@ -16,7 +13,9 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>{
             throw new IllegalArgumentException("capacity: " + capacity);
         }
         elementData = (E[]) new Object[capacity];
-        size = 0;
+        super(size) = 0;
+        
+        //ATTENTION: you were transitioning all the size functions to the super class
     }
 
     // post: constructs an empty list of default capacity
@@ -24,10 +23,6 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>{
         this(DEFAULT_CAPACITY);
     }
 
-    // post: returns the current number of elements in the list
-    public int size() {
-        return size;
-    }
 
     // pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
     // post: returns the value at the given index in the list
@@ -36,47 +31,12 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>{
         return elementData[index];
     }
 
-    // post: creates a comma-separated, bracketed version of the list
-    public String toString() {
-        if (size == 0) {
-            return "[]";
-        } else {
-            String result = "[" + elementData[0];
-            for (int i = 1; i < size; i++) {
-                result += ", " + elementData[i];
-            }
-            result += "]";
-            return result;
-        }
-    }
-
-    // post : returns the position of the first occurrence of the given
-    //        value (-1 if not found)
-    public int indexOf(E value) {
-        for (int i = 0; i < size; i++) {
-            if (elementData[i].equals(value)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    // post: returns true if list is empty, false otherwise
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    // post: returns true if the given value is contained in the list,
-    //       false otherwise
-    public boolean contains(E value) {
-        return indexOf(value) >= 0;
-    }
 
     // post: appends the given value to the end of the list
     public void add(E value) {
         ensureCapacity(size + 1);
         elementData[size] = value;
-        size++;
+        super(size)++;
     }
 
     // pre : 0 <= index <= size() (throws IndexOutOfBoundsException if not)
@@ -91,42 +51,16 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>{
             elementData[i] = elementData[i - 1];
         }
         elementData[index] = value;
-        size++;
+        super(size)++;
     }
  //------------------------------------------------Harold's Methods Start-----------------------------------------------------------
-    // pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
-    // post: removes value at the given index, shifting subsequent values left
-    public void remove(int index) {
-        checkIndex(index);
-        for (int i = index; i < size - 1; i++) {
-            elementData[i] = elementData[i + 1];
-        }
-        elementData[size - 1] = null;
-        size--;
-    }
-    
- ----
+ 
 
     // pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
     // post: replaces the value at the given index with the given value
     public void set(int index, E value) {
         checkIndex(index);
         elementData[index] = value;
-    }
-
-    // post: list is empty
-    public void clear() {
-        for (int i = 0; i < size; i++) {
-            elementData[i] = null;
-        }
-        size = 0;
-    }
-
-    // post: appends all values in the given list to the end of this list
-    public void addAll(List<E> other) {
-        for (E value: other) {
-            add(value);
-        }
     }
 
     // post: returns an iterator for this list
@@ -146,13 +80,7 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>{
         }
     }
 
-    // post: throws an IndexOutOfBoundsException if the given index is
-    //       not a legal index of the current list
-    private void checkIndex(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("index: " + index);
-        }
-    }
+
 
      //------------------------------------------------Andy's Methods Start-----------------------------------------------------------
     private class ArrayListIterator implements Iterator<E> {
