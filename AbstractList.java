2a556eeaa3a749008e1c23c581f690125ca7ddfb
Harold Pham <63668080+HaroldPham@users.noreply.github.com>
2020-05-25 13:49:03 -0700

    Cleaning Update
    - Removed Unnecessary inner class
    - Removed Test comments 
    - Elaborated Header and comments.

--- AbstractList.java
+++ AbstractList.java
@@ -1,68 +1,76 @@
-/*CS211 SPRING 2020, Created: 5/20/20, Lee Janzen, Harold Pham
-This is the abstract list class.
+/*CS211 SPRING 2020, Created: 5/20/20, Lee Janzen, Harold Pham, Andy Zhong
+This is the abstract list class that ArrayList and linkedList both extend.
 Created for ch16, programming project 3.*/
 import java.util.*;
 
 public abstract class AbstractList<E> implements List<E> {
-   protected int size;        // current number of elements in the list
+    protected int size;        // current number of elements in the list
    
-   //test 1
     // post: returns true if list is empty, false otherwise
-    public boolean isEmpty() {
+    public boolean isEmpty() 
+    {
         return size == 0;
     }
     
-    //test 2
     // post: returns the current number of elements in the list
-    public int size() {
+    public int size() 
+    {
         return size;
     }
     
-    // post: returns the current number of elements in the list + 1 increment
-    public int increaseSize() {
+    // post: returns the current number of elements in the list + 1 increment while increasing the size by 1.
+    public int increaseSize() 
+    {
         size++;
         return size;
     }
     
-    // post: returns the current number of elements in the list - 1 increment
-    public int decreaseSize() {
+    // post: returns the current number of elements in the list - 1 increment while decreasing the size by 1.
+    public int decreaseSize() 
+    {
         size--;
         return size;
     }
     
-    //test 3
-    // post : returns the position of the first occurrence of the given
-    //        value (-1 if not found)
-    public int indexOf(E value) {
+    // post : returns the position of the first occurrence of the given value (-1 if not found)
+    public int indexOf(E value) 
+    {
         Iterator<E> i = this.iterator();
         int index = 0;
 
-        while (i.hasNext()){
-             if (i.next() == value){
+        while (i.hasNext())
+        {
+             if (i.next() == value)
+             {
                  return index;
-             } else {
+             } 
+             else 
+             {
                  index++;
              }
          }
          return -1;
     }
 
-   //test 4
-    // post: returns true if the given value is contained in the list,
-    //       false otherwise
-    public boolean contains(E value) {
+    // post: returns true if the given value is contained in the list, false otherwise.  
+    public boolean contains(E value) 
+    {
         return indexOf(value) >= 0;
     }
     
-    //test 5
     // post: creates a comma-separated, bracketed version of the list
-    public String toString() {
-        if (size == 0) {
+    public String toString() 
+    {
+        if (size == 0) 
+        {
             return "[]";
-        } else {
+        } 
+        else 
+        {
             Iterator<E> i = this.iterator();
             String result = "[" + i.next();
-            while(i.hasNext()) {
+            while(i.hasNext()) 
+            {
                 result += ", " + i.next();
             }
             result += "]";
@@ -70,18 +78,12 @@ public abstract class AbstractList<E> implements List<E> {
         }
     }
     
-    
-    
-    
-    // post: throws an IndexOutOfBoundsException if the given index is
-    //       not a legal index of the current list
-    protected void checkIndex(int index) {
-        if (index < 0 || index >= size()) {
+    // post: throws an IndexOutOfBoundsException if the given index is not a legal index of the current list
+    protected void checkIndex(int index) 
+    {
+        if (index < 0 || index >= size()) 
+        {
             throw new IndexOutOfBoundsException("index: " + index);
         }
-    }
-
-    
-    private abstract class AbstractListIterator<E> implements Iterator<E> {
     }
 }
